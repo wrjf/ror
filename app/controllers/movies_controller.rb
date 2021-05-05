2b774303d11104d02468d58c1c23c9105c08040a
EC2 Default User <ec2-user@ip-172-31-34-222.us-east-2.compute.internal>
2021-05-05 07:56:21 +0000

    done

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,30 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    sort = params[:sort] || session[:sort]
+    case sort
+    when 'title'
+      ordering, @title_header = {:title=>:asc},'bg-warning hilite'
+    when 'release_date'
+      ordering, @date_header = {:release_date=>:asc}, 'bg-warning hilite'
+    end
+    @all_ratings = Movie.all_ratings()
+    @ratings_to_show = Movie.with_ratings(params[:rating])
+    @selected_ratings = params[:ratings] #|| session[:ratings] || {}
+    
+    if @selected_ratings.nil?
+      session[:sort] = {}
+      session[:ratings] = {}
+      @selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}]
+    end
+    
+    
+    if (params[:sort] != session[:sort]) or (params[:ratings] != session[:ratings])
+      session[:sort] = sort
+      session[:ratings] = @selected_ratings
+      redirect_to :sort => sort, :ratings => @selected_ratings and return
+    end
+    @movies = Movie.where(rating: @selected_ratings.keys).order(ordering)
   end
 
   def new
@@ -23,7 +46,12 @@ class MoviesController < ApplicationController
   def edit
     @movie = Movie.find params[:id]
   end
-
+  
+  
+  def search
+    @movie = Movie.find params[:m_r]
+  end
+  
   def update
     @movie = Movie.find params[:id]
     @movie.update_attributes!(movie_params)
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,10 @@
 class Movie < ActiveRecord::Base
+    def self.all_ratings
+        ['G','PG','PG-13','R','NC-17','X','XX']
+    end
+    
+    def self.with_ratings(ratings)
+        Movie.where(rating: ratings)
+    end
+        
 end
